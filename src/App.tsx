aa31f8372dfcd09ac8feaa956a1a951986b9ea6a
Kübra Demirgüç <kubradmrgc965@gmail.com>
2026-02-24 14:42:42 +0300

    feat: resolve line ending warnings and update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ function App() {
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Web Tasarımı ve Programlama</h1>
       <h2>LAB-1 Uygulaması</h2>
-      <p><strong>Ad Soyad:</strong> [Adın Soyadın]</p>
-      <p><strong>Öğrenci No:</strong> [Numaran]</p>
-      <p><em>Ege Üniversitesi - Bilgisayar Mühendisliği</em></p>
+      <p><strong>Ad Soyad:</strong> [Kübra DEMİRGÜÇ]</p>
+      <p><strong>Öğrenci No:</strong> [230541115]</p>
+      <p><em>Fırat Üniversitesi - Yazılım Mühendisliği</em></p>
     </div>
   )
 }
