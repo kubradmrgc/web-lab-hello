17295e55a451fa18163f984ea3f6a958e3be4808
Kübra Demirgüç <kubradmrgc965@gmail.com>
2026-02-24 12:54:41 +0300

    chore: initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Web Tasarımı ve Programlama</h1>
+      <h2>LAB-1 Uygulaması</h2>
+      <p><strong>Ad Soyad:</strong> [Adın Soyadın]</p>
+      <p><strong>Öğrenci No:</strong> [Numaran]</p>
+      <p><em>Ege Üniversitesi - Bilgisayar Mühendisliği</em></p>
+    </div>
+  )
+}
+export default App
